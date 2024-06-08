9323895292e5adff49f0207e344803427bc436ad
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-06-08 23:14:47 +0700

    feat: thêm API Diamond
    -Search by ID, Origin
    -CRUD

---
+++ DiamondShopSystem(SWD392)/Application/ViewModels/DiamondDTOs/DiamondDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.DiamondDTOs
+{
+    public class DiamondDTO
+    {
+        public int Id {  get; set; }
+        public string OriginName { get; set; }
+        public float CaratWeight { get; set; }
+        public string ClarityName { get; set; }
+        public string CutName { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ DiamondShopSystem(SWD392)/Application/ViewModels/DiamondDTOs/UpdateDiamondDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.ViewModels.DiamondDTOs
+{
+    public class UpdateDiamondDTO
+    {
+        public string OriginName { get; set; }
+        public float CaratWeight { get; set; }
+        public string ClarityName { get; set; }
+        public string CutName { get; set; }
+        public string Color { get; set; }
+        public decimal Price { get; set; }
+    }
+}

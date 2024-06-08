75fd6c8fc7ffd8b1b59f55f32e53ca479029b5ab
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-06-08 18:48:53 +0700

    feat : add validation cho Account

---
+++ DiamondShopSystem(SWD392)/DiamondShopSystem(SWD392)/Validations/AccountValidations/UpdateAccountViewModelValidation.cs
@@ -0,0 +1,20 @@
+﻿using Application.ViewModels.AccountDTOs;
+using FluentValidation;
+
+namespace DiamondShopSystem_SWD392_.Validations.AccountValidations
+{
+    public class UpdateAccountViewModelValidation : AbstractValidator<UpdateAccountDTO>
+    {
+        public UpdateAccountViewModelValidation()
+        {
+
+            RuleFor(x => x.Name).NotEmpty().MinimumLength(2);
+            RuleFor(x => x.Email).NotEmpty().EmailAddress().Must(email => email.EndsWith("@gmail.com"))
+                .WithMessage("Email must have the extension @gmail.com");
+            RuleFor(x => x.PhoneNumber).NotEmpty().Matches(@"^0[0-9]{9}$")
+                .WithMessage("The phone number must have 10 digits and start with 0");
+            RuleFor(x => x.Password).NotEmpty().MinimumLength(8)
+                .WithMessage("Password must be at least 8 characters long");
+        }
+    }
+}

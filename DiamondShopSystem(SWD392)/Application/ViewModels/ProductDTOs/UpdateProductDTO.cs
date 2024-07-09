2766b9a1d44bfd8171d3b1c249c8a54be0a1b114
haocetu <nhathao230502@gmail.com>
2024-07-09 15:51:46 +0700

    quantity product

--- DiamondShopSystem(SWD392)/Application/ViewModels/ProductDTOs/UpdateProductDTO.cs
+++ DiamondShopSystem(SWD392)/Application/ViewModels/ProductDTOs/UpdateProductDTO.cs
@@ -8,12 +8,6 @@ namespace Application.ViewModels.ProductDTOs
 {
 	public class UpdateProductDTO
 	{
-		public string Name { get; set; }
-		public decimal Size { get; set; }
-		public decimal Price { get; set; }
-		public decimal Wage { get; set; }
-		public int ProductTypeId { get; set; }
-		public int CategoryId { get; set; }
 		public int Quantity { get; set; }
 	}
 }

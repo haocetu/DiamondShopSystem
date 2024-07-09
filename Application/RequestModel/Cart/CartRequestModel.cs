cb8b7b639e633a9fb5b3d034fed855824fd2a5f0
haocetu <nhathao230502@gmail.com>
2024-07-10 00:44:39 +0700

    ROLLBACK MAIN

--- Application/RequestModel/Cart/CartRequestModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Application.RequestModel.CartItem;
-
-namespace Application.RequestModel.Cart
-{
-    public class CartRequestModel
-    {
-        public ICollection<CartItemRequestModel> Items { get; set; }
-    }
-}
--- Application/RequestModel/CartItem/CartItemRequestModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Application.RequestModel.CartItem
-{
-    public class CartItemRequestModel
-    {
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
-    }
-}

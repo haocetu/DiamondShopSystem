4c96d425f4fc5319ee3c92d0cfc20de6ab26ae73
haocetu <nhathao230502@gmail.com>
2024-07-16 00:36:32 +0700

    validate

--- Application/ViewModels/ProductDTOs/CreateProductDTO.cs
+++ Application/ViewModels/ProductDTOs/CreateProductDTO.cs
@@ -14,14 +14,19 @@ namespace Application.ViewModels.ProductDTOs
 		[Required]
 		public string Name { get; set; }
 		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "CategoryId must be greater than 0.")]
 		public int CategoryId { get; set; }
 		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "ProductTypeId must be greater than 0.")]
 		public int ProductTypeId { get; set; }
 		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "Weight must be greater than 0.")]
 		public decimal Weight { get; set; }
 		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "Wage must be greater than 0.")]
 		public decimal Wage { get; set; }
 		[Required]
+		[Range(1, int.MaxValue, ErrorMessage = "Quantity must be greater than 0.")]
 		public int Quantity { get; set; }
 		public List<int> PrimaryDiamonds { get; set; }
 		public List<int> SubDiamonds { get; set; }

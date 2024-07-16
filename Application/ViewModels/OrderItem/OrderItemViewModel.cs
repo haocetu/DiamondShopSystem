e2be4aeaed54487e9bb93d2a969df0db7c1c32f1
hauzanq <giangthse173685@fpt.edu.vn>
2024-07-16 10:21:42 +0700

    fix(order-service): map field for order viewmodel

--- Application/ViewModels/OrderItem/OrderItemViewModel.cs
+++ Application/ViewModels/OrderItem/OrderItemViewModel.cs
@@ -5,5 +5,8 @@
         public int ProductId { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
+        public string WarrantyDescription { get; set; }
+        public DateTime WarrantyStartDate { get; set; }
+        public DateTime WarrantyEndDate { get; set; }
     }
 }

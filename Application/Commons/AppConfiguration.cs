c5d23eceb568126cc631dad768f4eb46047634a1
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-07-19 19:49:42 +0700

    fix: fix firebase

--- Application/Commons/AppConfiguration.cs
+++ Application/Commons/AppConfiguration.cs
@@ -19,12 +19,26 @@ namespace Application.Commons
         public string PAYOS_API_KEY { get; set; } = string.Empty;
         public string PAYOS_CLIENT_ID { get; set; } = string.Empty;
     }
-
+    public class GoogleImage
+    {
+        public string type { get; set; }
+        public string project_id { get; set; }
+        public string private_key_id { get; set; }
+        public string private_key { get; set; }
+        public string client_email { get; set; }
+        public string client_id { get; set; }
+        public string auth_uri { get; set; }
+        public string token_uri { get; set; }
+        public string auth_provider_x509_cert_url { get; set; }
+        public string client_x509_cert_url { get; set; }
+        public string universe_domain { get; set; }
+    }
     public class AppConfiguration
     {
         public string DatabaseConnection { get; set; }
         public JWTSection JWTSection { get; set; }
         public PayOSConfig PayOSConfig { get; set; }
+        public GoogleImage GoogleImage { get; set; }
 
     }
 

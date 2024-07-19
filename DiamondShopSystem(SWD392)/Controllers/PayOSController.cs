0d7e202d67189a15aa638b333f50fed0e9f47f54
Nguyễn Anh Duy <duynguyenbt21093@gmail.com>
2024-07-19 18:21:50 +0700

    fix: fix api payos use for cart

--- DiamondShopSystem(SWD392)/Controllers/PayOSController.cs
+++ DiamondShopSystem(SWD392)/Controllers/PayOSController.cs
@@ -10,23 +10,25 @@ namespace DiamondShopSystem_SWD392_.Controllers
     {
         private readonly PayOS _payOS;
         private readonly IOrderService _orderService;
+        private readonly ICartService _cartService;
         private readonly IProductService _productService;
-        public PayOSController(PayOS payOS, IOrderService orderService, IProductService productService)
+        public PayOSController(PayOS payOS, IOrderService orderService, IProductService productService, ICartService cartService)
         {
             _payOS = payOS;
             _orderService = orderService;
             _productService = productService;
+            _cartService = cartService;
         }
-        [HttpPost]
-        public async Task<IActionResult> Checkout([FromQuery] int orderId)
+        [HttpGet]
+        public async Task<IActionResult> Checkout()
         {
             try
             {
                 int orderCode = int.Parse(DateTimeOffset.Now.ToString("ffffff"));
                 List<ItemData> items = [];
-                var order = await _orderService.GetOrderDetailsAsync(orderId);
+               var cartUser = await _cartService.GetCartForUserAsync();
 
-                var productList = order.Data.Items;
+                var productList = cartUser.Data.Items;
 
                 foreach(var o in productList)
                 {
@@ -54,7 +56,7 @@ namespace DiamondShopSystem_SWD392_.Controllers
                 var cancelUrl = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf6Z7CgLJ3JfLy4IREsARVyxcBnQQnHN40jw&s";
 
                 // Tạo đối tượng PaymentData để gửi yêu cầu thanh toán
-                PaymentData paymentData = new PaymentData(orderCode, (int)order.Data.TotalPrice, "Thanh toán đơn hàng", items, cancelUrl, successUrl);
+                PaymentData paymentData = new PaymentData(orderCode, (int)cartUser.Data.TotalPrice, "Thanh toán đơn hàng", items, cancelUrl, successUrl);
                 CreatePaymentResult createPayment = await _payOS.createPaymentLink(paymentData);
 
                 return Ok(new
@@ -69,17 +71,17 @@ namespace DiamondShopSystem_SWD392_.Controllers
                 return Redirect("https://localhost:5001/swagger/index.html");
             }
         }
-        [HttpGet]
-        public async Task<IActionResult> Success( [FromQuery] int orderId, [FromQuery] string status)
-        {
-            if (status == "Cancel")
-            {
-                var cancel = await _orderService.ChangeOrderStatusAsync(orderId,"Cancel");
-                return Redirect("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf6Z7CgLJ3JfLy4IREsARVyxcBnQQnHN40jw&s");
-            }
-              await _orderService.ChangeOrderStatusAsync(orderId, "Paid");
-            return Redirect("https://icpih.com/media-intestinal-health-ihsig/PAYMENT-SUCCESS.png");
-        }
+        //[HttpGet]
+        //public async Task<IActionResult> Success( [FromQuery] int orderId, [FromQuery] string status)
+        //{
+        //    if (status == "Cancel")
+        //    {
+        //        var cancel = await _orderService.ChangeOrderStatusAsync(orderId,"Cancel");
+        //        return Redirect("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTf6Z7CgLJ3JfLy4IREsARVyxcBnQQnHN40jw&s");
+        //    }
+        //      await _orderService.ChangeOrderStatusAsync(orderId, "Paid");
+        //    return Redirect("https://icpih.com/media-intestinal-health-ihsig/PAYMENT-SUCCESS.png");
+        //}
     }
 }
 
